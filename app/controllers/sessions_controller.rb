eadddcb5e6a4d74bb62d5082f08797425d603536
tsmliu213 <tsmliu213@gmail.com>
2016-08-04 22:47:53 -0700

    Implement basic login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Log the user in and redirect to the user's show page.
+      log_in user
+      redirect_to user
     else
       # Create an error message.
       flash.now[:danger] = 'Invalid email/password combination'
@@ -14,6 +15,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    log_out
+    redirect_to root_url
   end
 
 end
