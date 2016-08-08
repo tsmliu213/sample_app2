72c5db1ff75c93ee7eb6140b8830f23d823aa1fd
tsmliu213 <tsmliu213@gmail.com>
2016-08-07 21:19:47 -0700

    checkbox added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      remember user
       redirect_to user
     else
       # Create an error message.
@@ -15,7 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -18,8 +18,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
   test "login with valid information followed by logout" do
     get login_path
-    post login_path, params: { session: { email:    @user.email,
-                                          password: 'password' } }
+    post login_path, session: { email: @user.email, password: 'password' }
     assert is_logged_in?
     assert_redirected_to @user
     follow_redirect!
@@ -30,6 +29,8 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
+    # Simulate a user clicking logout in a second window.
+    delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,      count: 0
